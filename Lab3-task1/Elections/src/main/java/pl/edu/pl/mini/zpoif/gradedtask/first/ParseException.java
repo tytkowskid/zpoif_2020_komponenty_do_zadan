97207bffc3673a3aac84e424e9af0cffd2dad626
Maciej Bednarz <m.bednarz@mini.pw.edu.pl>
2020-10-23 15:16:15 +0200

    Inicjalne elementy do zadań.

---
+++ Lab3-task1/Elections/src/main/java/pl/edu/pl/mini/zpoif/gradedtask/first/ParseException.java
@@ -0,0 +1,10 @@
+package pl.edu.pl.mini.zpoif.gradedtask.first;
+
+public class ParseException extends Exception {
+
+	private static final long serialVersionUID = 3145383248434636943L;
+
+	public ParseException(String message) {
+		super(message);
+	}
+}
